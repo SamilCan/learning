a5b87fd8c09c57924c48e9ad32a5518eb7c25333
Gungor, Erdi <Erdi.Gungor@sony.com>
2018-10-15 17:19:05 +0300

    dene

--- mnist_1.0_softmax.py
+++ mnist_1.0_softmax.py
@@ -20,7 +20,7 @@ print("Tensorflow version " + tf.__version__)
 tf.set_random_seed(0)
 
 <<<<<<< HEAD
-# neural network with 1 layer of 10 softmax neurons.e
+# neural network with 1 layer of 10 softmax neurons.eer
 =======
 # neural network with 1 layer of 10 softmax neurons_a
 >>>>>>> deneme
